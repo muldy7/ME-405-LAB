5ea4b2110383e9fe326eb72870d85da21e7a9c91
Abraham Muldrow <abe@muldrow.net>
2024-01-11 17:13:29 -0800

    test

--- lab0/docs/main.py
+++ lab0/docs/main.py
@@ -1,1 +1,12 @@
+# from matplotlib import pyplot
+import time
 
+adc0 = pyb.ADC(pyb.Pin.board.PC0)
+y = []
+for x in range(100):
+    time.sleep(0.1)
+    y.append(adc0.read())
+    print(y[x])
+    
+print(y)
+    
