3fcf8909924e22b51c8af8a53e41bd43959431bf
Abraham Muldrow <abe@muldrow.net>
2024-01-18 15:23:12 -0800

    changed lab

--- lab0/docs/lab_00b.py
+++ lab0/docs/lab_00b.py
@@ -1,6 +1,7 @@
-
 import micropython
 import time
+import cqueue
+
  
 
 def main():
@@ -11,12 +12,15 @@ def main():
     pinC0 = pyb.Pin(pyb.Pin.board.PC0, pyb.Pin.OUT_PP)  #initializes the pin as an outport pin
     #need to intialize the output pin i think in the same way
     
-    time_int=pyb.Timer (1,freq = 1000)      # timer 1 running at 1000 Hz
-    timer.counter()                         # gets the timer value
+    pinB0 = pyb.Pin (pyb.Pin.board.PB0, pyb.Pin.IN)
+    
+    time_int = pyb.Timer (1,freq = 1000)      # timer 1 running at 1000 Hz
+    time_int.counter()                         # gets the timer value
     micropython.alloc_emergency_exception_buf(100) # alocates buffer for emergency exception handling, used when memory is a constraint
     
     QUEUE_SIZE = 2000                               # this is the size of the que to collect 2 seconds of data at 1 ms interupts
     int_queue = cqueue.IntQueue(QUEUE_SIZE)
+    step_response()
     
     
     
@@ -28,7 +32,7 @@ def timer_int(tim_num):
     """
   
     #COLLECT ADC
-    int_queue.put()  #read and put into queue. inside the put() is the value that will be read from the pin. this has not been set up in the code yet
+    int_queue.put(pinB0.value())  #read and put into queue. inside the put() is the value that will be read from the pin. this has not been set up in the code yet
     int_queue.full()
     if value.int_queue.full() == True:
             timer_int.callback(None)          #If que is full, disable Callback 
@@ -40,22 +44,20 @@ def timer_int(tim_num):
 
 
 def step_response():# run this function when requeste dby user or through GUI
-      """!
-      Doxygen style docstring for this function 
-      """
-      # Function code here
-time_int.callback (timer_int)  #configure and enable the calllback. example : timmy.callback(timer_cb)
-pinc0.high()                   #set the trigger pin to high pin.high   (pinC0.value(1)
-while not int_que.full():                             #wait for a full queue ( while not my_que.full()
-        pass
-                        #     pass
-        pinc0.high ()                 #	Set pin back to low
-                        
-                                 
-while not int_que.empty():          #iterate through the queue to and add to the array output_array
+    """!
+    Doxygen style docstring for this function 
+    """
+    # Function code here
+    timer_int.callback(time_int)  #configure and enable the calllback. example : timmy.callback(timer_cb)
+    pinC0.high()                   #set the trigger pin to high pin.high   (pinC0.value(1)
+    while not int_que.full():                             #wait for a full queue ( while not my_que.full()
+            pass						#     pass
+            pinC0.high ()                 #	Set pin back to low
+                                    
+    while not int_que.empty():          #iterate through the queue to and add to the array output_array
         output=int_que.get()
         output_array.append(output)
-print('done')
+    print('done')
 
 
 if __name__ == "__main__":
