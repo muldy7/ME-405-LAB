aa4e80211e3aa290e55d946c7e021e69e9fa0ec7
Abraham Muldrow <abe@muldrow.net>
2024-01-19 17:12:35 -0800

    friday work
    working on reading values from the board and then graphing those values

--- lab0/src/lab0_week2.py
+++ lab0/src/lab0_week2.py
@@ -4,22 +4,43 @@
 import serial
 from serial import Serial
 import time
+from matplotlib import pyplot as plt
 
 
 ser = serial.Serial("COM4", 9600)
 
-for i in range(4):	# will have to test if there's data
-    try:
-        cc=str(ser.readline())
-        print(cc)
-    finally:
-        pass
 
-print("writing to board")
-ser.write(bytearray('\x03','ascii')) # ascii code for ctrl+c
-ser.write(bytearray('\x02','ascii')) # ctrl+b
-ser.write(bytearray('\x04','ascii')) # ctrl+d
-# ser = serial.Serial("COM11", 9600)
-while True:
-     cc=str(ser.readline())	# read data from the serial port
-     print(cc)
+try:
+    cc=str(ser.readline()) # this has way to many values for things that arent numbers
+    print(cc) # data is read as a string
+    cc.split(",") # can split string into a list
+    
+    print(cc[1])
+    
+#     fig, ax = plt.subplots()
+#     t = range(len(cc))
+#     ax.plot(t, cc)  # create a plot of the filtered data
+#     ax.set_title("Plot for HW0")    # create a title for the plot
+#     plt.show() 
+
+finally:
+    pass
+
+
+# for i in range(4):	# will have to test if there's data
+#     try:
+#         cc=str(ser.readline())
+#         print(cc)
+#         time.sleep(5)
+#     finally:
+#         pass
+
+# print("writing to board")
+# ser.write(bytearray('\x03','ascii')) # ascii code for ctrl+c
+# ser.write(bytearray('\x02','ascii')) # ctrl+b
+# ser.write(bytearray('\x04','ascii')) # ctrl+d
+# # ser = serial.Serial("COM11", 9600)
+# 
+# while True:
+#      cc=str(ser.readline())	# read data from the serial port
+#      print(cc)
--- lab0/src/test.py
+++ lab0/src/test.py
@@ -1,16 +1,29 @@
+# import micropython
 import micropython
 import time
+import math
+#import matplotlib
+#from matplotlib import pyplot as plt
 
-pinC0 = pyb.Pin (pyb.Pin.board.PC0, pyb.Pin.OUT_PP)
-time.sleep(2)
-pinC0.high()
-time.sleep(5)
-pinC0.low()
+t = []
+for i in range(100):
+    t.append(i*10)
 
-# pinC0.high()
-# pinB0 = pyb.Pin(pyb.Pin.board.PB0, pyb.Pin.IN)
-# if pinB0.value():
-#     print("good")
-#     print(pinB0.value())
-#     
-#     pinC0.low()
+v_max = 3.03
+r = 100
+c = 3.3
+data = []
+
+for i in t:
+    value = (3.03*(1-math.exp(-i/(r*c))))
+    data.append(value)
+    
+while True:
+    print(data)
+    time.sleep(5)
+
+
+# fig, ax = plt.subplots() 
+# ax.plot(t, data)  # create a plot of the filtered data
+# ax.set_title("Plot for HW0")    # create a title for the plot
+# plt.show() 
