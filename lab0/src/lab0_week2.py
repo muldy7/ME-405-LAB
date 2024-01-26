104b24831bc59abeed7558b5890f6fbde4c5c3c0
lrambo1 <107410442+lrambo1@users.noreply.github.com>
2024-01-25 16:58:13 -0800

    Update lab0_week2.py

--- lab0/src/lab0_week2.py
+++ lab0/src/lab0_week2.py
@@ -82,23 +82,3 @@ pyplot.show()
 
 
 
-
-
-
-# for i in range(4):	# will have to test if there's data
-#     try:
-#         cc=str(ser.readline())
-#         print(cc)
-#         time.sleep(5)
-#     finally:
-#         pass
-
-# print("writing to board")
-# ser.write(bytearray('\x03','ascii')) # ascii code for ctrl+c
-# ser.write(bytearray('\x02','ascii')) # ctrl+b
-# ser.write(bytearray('\x04','ascii')) # ctrl+d
-# # ser = serial.Serial("COM11", 9600)
-# 
-# while True:
-#      cc=str(ser.readline())	# read data from the serial port
-#      print(cc)
