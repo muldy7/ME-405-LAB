f2707ac12f52074537eaa055a3c75d9b8e064454
Ptomson <86498800+Ptomson@users.noreply.github.com>
2024-01-25 16:54:41 -0800

    Thursday Work
    Ye

--- lab0/src/lab0_week2.py
+++ lab0/src/lab0_week2.py
@@ -4,27 +4,85 @@
 import serial
 from serial import Serial
 import time
-from matplotlib import pyplot as plt
+import math
+from matplotlib import pyplot 
+
+ser = serial.Serial("COM5", 9600)
+
+print("sending to board")
+ser.write(bytearray('\x03','ascii')) # ascii code for ctrl+c
+ser.write(bytearray('\x02','ascii')) # ctrl+b
+ser.write(bytearray('\x04','ascii')) # ctrl+d
 
 
-ser = serial.Serial("COM4", 9600)
+
+array =[]
+cont = 1
+time=[]
+volts=[]
+while cont == 1:
+#      line=ser.readline().decode('utf-8').strip()	# read data from the serial port
+#      print(line)
+    value = ser.readline().decode('utf-8').strip()
+    if value == 'end':
+        cont = 0
+    else:
+        array.append(value)
+for i in array:
+    index = i.split(',')
+    try:  
+        timeval  =float(index[0].strip('('))
+        voltval = float(index[1].strip(')'))
+        time.append(timeval)
+        volts.append(voltval)
+    except:
+        pass
+#print(volts)
+       
 
 
-try:
-    cc=str(ser.readline()) # this has way to many values for things that arent numbers
-    print(cc) # data is read as a string
-    cc.split(",") # can split string into a list
+
+
+#Simulated response Plot - This works
+t = []
+for i in range(len(time)):
+    t.append(2*i)
+
+v_max = 3.03
+r = 100
+c = 3.3
+data = []
+
+for i in t:
+    value = (3.03*(1-math.exp(-i/(r*c))))
+    data.append(value)
     
-    print(cc[1])
-    
-#     fig, ax = plt.subplots()
-#     t = range(len(cc))
-#     ax.plot(t, cc)  # create a plot of the filtered data
-#     ax.set_title("Plot for HW0")    # create a title for the plot
-#     plt.show() 
+print(data)
 
-finally:
-    pass
+
+pyplot.plot(t, volts, label = 'Measured Response Data', color = 'deepskyblue', marker = '.' )
+pyplot.plot(t, data, label = 'Simulated Data', color = 'black', linestyle = '-')
+pyplot.xlabel('Time [ms]')
+pyplot.ylabel('Volts [v]')
+pyplot.title('Simulated and Measured Step Response')
+pyplot.legend()
+pyplot.axis([0, 2000, 0, 3.5])
+pyplot.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # for i in range(4):	# will have to test if there's data
--- lab0/src/test.py
+++ lab0/src/test.py
@@ -1,12 +1,11 @@
 # import micropython
-import micropython
 import time
 import math
-#import matplotlib
-#from matplotlib import pyplot as plt
+import matplotlib
+from matplotlib import pyplot as plt
 
 t = []
-for i in range(100):
+for i in range(250):
     t.append(i*10)
 
 v_max = 3.03
@@ -18,12 +17,11 @@ for i in t:
     value = (3.03*(1-math.exp(-i/(r*c))))
     data.append(value)
     
-while True:
-    print(data)
-    time.sleep(5)
+print(data)
+   # time.sleep(5)
 
 
-# fig, ax = plt.subplots() 
-# ax.plot(t, data)  # create a plot of the filtered data
-# ax.set_title("Plot for HW0")    # create a title for the plot
-# plt.show() 
+fig, ax = plt.subplots() 
+ax.plot(t, data)  # create a plot of the filtered data
+ax.set_title("Plot for HW0")    # create a title for the plot
+plt.show() 
